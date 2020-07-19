b04ad6f5ccace72515355dc31914a0769f7fb8e0
vmwmaster <wvilladiego@hotmail.com>
2020-07-19 16:02:43 -0500

    mi primer commit

---
+++ app/src/main/java/com/wilson/miapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wilson.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
